f3cbf493b0677804cc98d34e5abc866b120efe81
jeongjoonsup <penny4860@gmail.com>
2017-09-11 22:30:58 +0900

    code cleaning

--- main.py
+++ main.py
@@ -39,31 +39,39 @@ def get_image_points(images, n_pattern_rows=6, n_pattern_cols=8, show_pts=False)
 
 # Make a list of calibration images
 images = glob.glob('calibration_wide/GO*.jpg')
+
+# 1. Get image points
 img_points = get_image_points(images, n_pattern_rows=6, n_pattern_cols=8)
+
+# 2. Get object points
 obj_points = get_object_points(len(img_points), n_pattern_rows=6, n_pattern_cols=8)
 
-# Test undistortion on an image
+# 3. Do calibration for test image size
 img = cv2.imread('calibration_wide/test_image.jpg')
 img_size = (img.shape[1], img.shape[0])
-
-# Do camera calibration given object points and image points
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, img_size, None, None)
 
-
+# 4. Distortion correction using camera matrix & distortion coefficient
 dst = cv2.undistort(img, mtx, dist, None, mtx)
-cv2.imwrite('calibration_wide/test_undist.jpg',dst)
 
-# Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
-dist_pickle = {}
-dist_pickle["mtx"] = mtx
-dist_pickle["dist"] = dist
-pickle.dump( dist_pickle, open( "calibration_wide/wide_dist_pickle.p", "wb" ) )
-#dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
-# Visualize undistortion
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
-ax1.imshow(img)
-ax1.set_title('Original Image', fontsize=30)
-ax2.imshow(dst)
-ax2.set_title('Undistorted Image', fontsize=30)
-plt.show()
+print("Camera matrix")
+print(mtx)
+print("distortion coefficient")
+print(dist)
+
+# cv2.imwrite('calibration_wide/test_undist.jpg',dst)
+# 
+# # Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
+# dist_pickle = {}
+# dist_pickle["mtx"] = mtx
+# dist_pickle["dist"] = dist
+# pickle.dump( dist_pickle, open( "calibration_wide/wide_dist_pickle.p", "wb" ) )
+# #dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
+# # Visualize undistortion
+# f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20,10))
+# ax1.imshow(img)
+# ax1.set_title('Original Image', fontsize=30)
+# ax2.imshow(dst)
+# ax2.set_title('Undistorted Image', fontsize=30)
+# plt.show()
 
